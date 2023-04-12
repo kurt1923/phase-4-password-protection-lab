ba4b3269d477bed2597e57dcbbff27112769293c
Kurt Vermillion <kurtv0727@gmail.com>
2023-04-12 16:46:34 -0400

    password

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+    def show
+        @user = User.find_by(id: session[:user_id])
+        render json: user
+    end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: {error: "Invalid username or password"} , status: :unauthorized
+        end
+    end
+
+    private
+
+    def authorize
+        return render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :user_id
+    end
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+end
